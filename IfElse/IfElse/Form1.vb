5c174f6d0de0e50a286f3767580a991fd2fa01eb
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:14:47 -0500

    In-class exercise

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,20 +11,33 @@ Public Class Form1
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
-        num1 = CDbl(st1)
-        num2 = CDbl(st2)
-
-        Dim result As String
-
-        If num1 > num2 Then
-            result = "The larger number is " & num1
-        ElseIf num2 > num1 Then
-            result = "The larger number is " & num2
-        Else
-            result = "They are equal"
+        ' Checkpoint. 
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please type a number")
+            Return
         End If
 
-        txtResult.Text = result
+        'Clean area
+            num1 = CDbl(st1)
+            num2 = CDbl(st2)
+        ' Checkpoint
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please type a positive number")
+            Return
+        End If
+
+            Dim result As String
+
+            If num1 > num2 Then
+                result = "The larger number is " & num1
+            ElseIf num2 > num1 Then
+                result = "The larger number is " & num2
+            Else
+                result = "They are equal"
+            End If
+
+            txtResult.Text = result
+
     End Sub
 
 End Class
